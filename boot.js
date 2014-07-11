6f5e39e181b52f69114252c62cf0398be3634ebf
ruchiraw <ruchira.wageesha@gmail.com>
2014-07-11 09:45:04 +0530

    refactoring

--- boot.js
+++ boot.js
@@ -1,47 +1,30 @@
 var pager = require('page');
-var async = require('async');
 var dust = require('dust')();
+
 var serand = require('serand');
-var layout = require('./layout');
+var page = serand.page;
+var layout = serand.layout(require);
 
 //registering jquery, bootstrap etc. plugins
 require('upload');
-
-var page = function (path, fn) {
-    pager(path, (fn ? function (ctx) {
-        serand.emit('boot', 'page', ctx);
-        fn(ctx);
-    } : null));
-};
-
-var comps = JSON.parse(require('./component.json'));
-comps.local.forEach(function (comp) {
-    require(comp);
-});
-
-var current = function (path) {
-    var ctx = new pager.Context(window.location.pathname + window.location.search);
-    var route = new pager.Route(path);
-    route.match(ctx.path, ctx.params);
-    return ctx;
-};
+//init app
+serand.init(require);
 
 page('/', function (ctx) {
     layout('two-column')
         .area('#header')
-        .add('navigation')
+        .add('hub-navigation')
         .add('breadcrumb')
         .area('#right')
         .add('auto-search')
         .area('#middle')
-        .add('auto-listing')
         .render();
 });
 
 page('/vehicles/:id', function (ctx) {
     layout('single-column')
         .area('#header')
-        .add('navigation')
+        .add('hub-navigation')
         .add('breadcrumb')
         .area('#middle')
         .add('auto-details', {
@@ -53,7 +36,7 @@ page('/vehicles/:id', function (ctx) {
 page('/login', function (ctx) {
     layout('single-column')
         .area('#header')
-        .add('navigation')
+        .add('hub-navigation')
         .area('#middle')
         .add('user-login')
         .render();
@@ -62,7 +45,7 @@ page('/login', function (ctx) {
 page('/register', function (ctx) {
     layout('single-column')
         .area('#header')
-        .add('navigation')
+        .add('hub-navigation')
         .area('#middle')
         .add('user-register')
         .render();
@@ -71,7 +54,7 @@ page('/register', function (ctx) {
 page('/add', function (ctx) {
     layout('single-column')
         .area('#header')
-        .add('navigation')
+        .add('hub-navigation')
         .area('#middle')
         .add('auto-add')
         .render();
